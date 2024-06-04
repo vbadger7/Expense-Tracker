9fd3dd60d696ca1bb09a348c40a22d852fae1292
Jackson Phanvilay <jphanvilay.1@gmail.com>
2024-06-04 12:31:09 -0500

    Added updated styling on login form

--- routes/expenseRoutes.js
+++ routes/expenseRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const expenseController = require('../controllers/expenseController');
 
-router.get('./expenxeRoutes.js', expenseController.showExpenses);
-router.post('./expenxeRoutes.js', expenseController.addExpense);
+router.get('./expenseRoutes.js', expenseController.showExpenses);
+router.post('./expenseRoutes.js', expenseController.addExpense);
 
 module.exports = router;
