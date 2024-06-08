e2e268a6bc422ea14630ef1a5e195ea70677f09f
Jackson Phanvilay <jphanvilay.1@gmail.com>
2024-06-07 19:52:23 -0500

    update serverjs

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
-require('dotenv').config();
-const express = require('express');
-const session = require('express-session');
-const cookieParser = require('cookie-parser');
-const exphbs = require('express-handlebars');
-const db = require('./models');
-const authRoutes = require('./routes/authRoutes');
-const expenseRoutes = require('./routes/expenseRoutes');
-const path = require('path');
+require("dotenv").config();
+const express = require("express");
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
+const exphbs = require("express-handlebars");
+const db = require("./models");
+const authRoutes = require("./routes/authRoutes");
+const expenseRoutes = require("./routes/expenseRoutes");
+const path = require("path");
+
+const sequelize = require("./config/connection");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -14,52 +17,60 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
-app.use(session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: true,
-}));
+app.use(
+ session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true,
+ })
+);
 
-app.engine('handlebars', exphbs.engine({ extname: '.handlebars', defaultLayout: 'main', layoutsDir: path.join(__dirname, 'views/layouts'),
-viewsDir: path.join(__dirname, 'views') }));
-app.set('view engine', 'handlebars');
-app.set('views', path.join(__dirname, 'views'));
+app.engine(
+ "handlebars",
+ exphbs.engine({
+  extname: ".handlebars",
+  defaultLayout: "main",
+  layoutsDir: path.join(__dirname, "views/layouts"),
+  viewsDir: path.join(__dirname, "views"),
+ })
+);
+app.set("view engine", "handlebars");
+app.set("views", path.join(__dirname, "views"));
 
 // Serve static files
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 
 // Define a route
-app.get('/', (req, res) => {
-    res.render('login');
+app.get("/", (req, res) => {
+ res.render("login");
 });
 
-
-app.get('/signup', (req, res) => {
-    res.render('signup'); 
+app.get("/signup", (req, res) => {
+ res.render("signup");
 });
 
-app.get('/expense', (req, res) => {
-    res.render('expense'); 
+app.get("/expense", (req, res) => {
+ res.render("expense");
 });
 
-app.get('/dashboard', (req, res) => {
-    res.render('dashboard'); 
+app.get("/dashboard", (req, res) => {
+ res.render("dashboard");
 });
 
-app.post('/login', (req, res) => {
-    res.render('login');
+app.post("/login", (req, res) => {
+ res.render("login");
 });
 
-app.get('/favicon.ico', (req, res) => res.status(204));
+app.get("/favicon.ico", (req, res) => res.status(204));
 
-console.log('__dirname resolves to:', __dirname);
+console.log("__dirname resolves to:", __dirname);
 
-app.use('/auth', authRoutes);
-app.use('/expenses', expenseRoutes);
+app.use("/auth", authRoutes);
+app.use("/expenses", expenseRoutes);
 
 db.sequelize.sync().then(() => {
-    app.listen(PORT, () => {
-        console.log(`Server is running on http://localhost:${PORT}`);
-    });
+ app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+ });
 });
