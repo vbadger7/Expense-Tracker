99fd5741bca8317804cc93ce661260aeea51e11f
nicoleables <159223166+nicoleables@users.noreply.github.com>
2024-06-07 19:01:15 -0500

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -3,24 +3,15 @@ require('dotenv').config();
 
 let sequelize;
 
-if (process.env.DATABASE_URL) {
-    sequelize = new Sequelize(process.env.DATABASE_URL, {
-        dialect: 'postgres',
-        protocol: 'postgres',
-        dialectOptions: {
-            ssl: {
-                require: true,
-                rejectUnauthorized: false
-            }
-        }
-    });
+if (process.env.DB_URL) {
+    sequelize = new Sequelize(process.env.DB_URL);
 } else {
     sequelize = new Sequelize(
-        process.env.DB_DATABASE,
-        process.env.DB_USERNAME,
-        process.env.DB_PASSWORD,
+        process.env.DB_NAME,
+        process.env.DB_USER,
+        process.env.DB_PW,
         {
-            host: process.env.DB_HOST,
+            host: 'localhost',
             dialect: 'postgres',
         },
     );
